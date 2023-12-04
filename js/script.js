77420780eb2c7f53d2847689d97ccce59a84eff7
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-12-04 14:30:26 -0300

    sass

--- js/script.js
+++ js/script.js
@@ -2,17 +2,41 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
-/*==================== scroll sections active link ====================*/
+
+/*==================== Navbar "active"  effect while scrolling ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
 
-    /*==================== sticky navbar ====================*/
+            });
+        };
+    });
+    /*==================== Sticky Navbar ====================*/
+
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
 
     /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
 
