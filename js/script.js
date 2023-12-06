931e6d2caac02bdd51942a970b28bebb9b420141
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-12-06 16:36:55 -0300

    FRONTEND: CHECK

--- js/script.js
+++ js/script.js
@@ -42,5 +42,28 @@ window.onscroll = () => {
 
 /*==================== scroll reveal ====================*/
 
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000, 
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
 
 /*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Fullstack Web Developer', 'Data Analyst', 'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
