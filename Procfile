4d2b61444c8805a6cf92969fa3e3f2309aba9479
Raissa Azevedo <rhaii.azevedo@gmail.com>
2023-12-22 21:36:29 -0300

    gunicorn bug fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-gunicorn myport.wsgi:application
+gunicorn myport.wsgi:application --workers 3 --bind 0.0.0.0:8000 --access-logfile -
--- myport/wsgi.py
+++ myport/wsgi.py
@@ -10,8 +10,5 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myport.settings')
-
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
