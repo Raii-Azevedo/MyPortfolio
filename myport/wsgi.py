976b66da8943428a0ac8f5f69f1e8b5f1c450521
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-02 01:59:00 -0300

    wsgi ok

--- myport/wsgi.py
+++ myport/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for myport project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myport.settings')
-app = get_wsgi_application()
+application = get_wsgi_application()
