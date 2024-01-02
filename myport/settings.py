56202f177f4f5f1c3fa733b224890cd2b5328430
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-02 02:20:48 -0300

    static files build

--- myport/settings.py
+++ myport/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
