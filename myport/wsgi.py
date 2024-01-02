4512272e9d17f4a4acb40d9582cec4c40c3816fc
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-02 01:53:50 -0300

    wsgi correção

--- myport/wsgi.py
+++ myport/wsgi.py
@@ -11,4 +11,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myport.settings')
-application = get_wsgi_application()
+app = get_wsgi_application()
