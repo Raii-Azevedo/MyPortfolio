0b31cf31db55b88a2b0064dc5bd3f616985a81c5
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-02 02:24:41 -0300

    static URL

--- myport/settings.py
+++ myport/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
