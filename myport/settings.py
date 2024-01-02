2b85c61892e87280d634de9647ac4cf1cbe82733
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-02 02:29:04 -0300

    coreect settings

--- myport/settings.py
+++ myport/settings.py
@@ -137,8 +137,9 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Note the use of a tuple
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
