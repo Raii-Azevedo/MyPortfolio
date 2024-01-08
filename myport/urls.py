217f7c81f906e9a75d9cbda0efabec59bb9682c6
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-08 17:01:09 -0300

    urls config

--- myport/urls.py
+++ myport/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] 
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
