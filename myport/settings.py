74ce8e13dfd42ebf194a95d51100d2f4852dd315
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-08 14:32:16 -0300

    remove version of pilow & psycopg2, SK config

--- myport/settings.py
+++ myport/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u09vljgtlrbm=*im(=s$u3as=&o&^4if%qxu+=a17h2c&q4t*9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
