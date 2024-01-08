09ba376d5495a15f9806cd23c91ca4786ce14712
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-01-08 16:08:16 -0300

    wsgi config

--- myport/wsgi.py
+++ myport/wsgi.py
@@ -1,5 +1,8 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myport.settings')
-app = get_wsgi_application()
+
+application = Cling(MediaCling(get_wsgi_application()))
