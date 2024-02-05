985814c222ac520ac43ecc22399751e45cc42679
Raissa Azevedo <rhaii.azevedo@gmail.com>
2024-02-05 15:43:13 -0300

    ok

--- myport/settings.py
+++ myport/settings.py
@@ -40,10 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ckeditor',
-    'ckeditor_uploader',
     'core',
-    'imagekit',
 ]
 
 MIDDLEWARE = [
