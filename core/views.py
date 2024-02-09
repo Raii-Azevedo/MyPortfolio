cbc419dd2dd1d2a7d81532f3b80d905f0bb06a92
raissa <rhaii.azevedo@gmail.com>
2024-02-08 21:51:01 -0300

    Same site avoid cookie LAX config

--- core/views.py
+++ core/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import Home, About, Experience, Portfolio, Services, Jobs
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.mail import send_mail
 
 
 
@@ -27,28 +24,11 @@ def index(request):
 
     return render(request, 'index.html', context)
 
-def enviar_email(request):
-    if request.method == 'POST':
-        full_name = request.POST.get('full_name')
-        email_address = request.POST.get('email_address')
-        mobile_number = request.POST.get('mobile_number')
-        email_subject = request.POST.get('email_subject')
-        message = request.POST.get('message')
 
-        # Construa o conteúdo do e-mail
-        email_content = f"Nome: {full_name}\nE-mail: {email_address}\nNúmero de telefone: {mobile_number}\nAssunto: {email_subject}\nMensagem: {message}"
+def minha_view(request):
+    response = HttpResponse("Hello, world!")
+    
+    # Definindo o cookie com SameSite=None
+    response.set_cookie('XSRF-TOKEN', 'valor_do_cookie', samesite='None')
 
-        # Envie o e-mail
-        send_mail(
-            subject='Novo contato do formulário de contato',
-            message=email_content,
-            from_email=email_address,
-            recipient_list=['rhaii.azevedo@gmail.com'],
-            fail_silently=False,
-        )
-
-        # Retorne uma resposta ao cliente
-        return HttpResponse("O e-mail foi enviado com sucesso!")
-    else:
-        # Se a solicitação não for POST, retorne uma resposta de método não permitido
-        return HttpResponse("Método não permitido.", status=405)
+    return response
