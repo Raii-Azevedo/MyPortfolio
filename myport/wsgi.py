f21316b777c2334e70314c75337e961f0d4c6d53
Raissa <rhaii.azevedo@gmail.com>
2026-03-15 12:05:37 -0300

    remove dj static

--- myport/wsgi.py
+++ myport/wsgi.py
@@ -1,8 +1,6 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myport.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
